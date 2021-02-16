c00ff2005072bf79496f80853787fb33b1f0fd71
Abhinav <agrawal.abhinav95@gmail.com>
2021-02-16 23:54:11 +0530

    Designed backend for product sell

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,14 @@ router.get('/sign-up', userController.signUp);
 
 router.post('/create', userController.create);
 
+//Route: This will take user to a HTML form for filling the product details for sell!
+router.get('/showProductSellForm',passport.checkAuthentication, userController.showProductSellForm)
+
+//Route: posting the new product for sell.
+router.post('/createPost', userController.createPost);
+
+router.post('/postUpload', userController.postUpload);
+
 //use passport as middleware too authenticate
 router.post('/createSession',passport.authenticate(
     'local',
@@ -19,5 +27,9 @@ router.post('/createSession',passport.authenticate(
 router.get('/sign-out', userController.destroySession);
 
 // router.post('/createPost', userController.postUpload)
+
+
+
+
 
 module.exports = router; 
